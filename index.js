518f70f7b39739737746cf09dcd41d553f01bcd4
tgmike <tgmike77@gmail.com>
2021-07-04 17:13:15 -0700

    Changed server to use better practices and added three html pages and a css file

--- index.js
+++ index.js
@@ -3,9 +3,82 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) =>{
-    if(req.url === '/'){
-        res.end('<h1>Homepage</h1>');
+    // if(req.url === '/'){
+    //     fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) =>{
+    //         res.writeHead(200, {'Content-Type': 'text/html'});  
+    //         res.end(content);
+    //     })
+        
+    // }
+    // if(req.url === '/about'){
+    //     fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) =>{
+    //         res.writeHead(200, {'Content-Type': 'text/html'});  
+    //         res.end(content);
+    //     })
+        
+    // }
+    // if(req.url === '/api/users'){
+    //     const users = [
+    //         {name: 'John Doe', age: 40},
+    //         {name: 'Bob Smith', age: 36}
+
+    //     ];
+    //     res.writeHead(200, {'Content-Type': 'application/json'});  
+    //     res.end(JSON.stringify(users));
+    // }
+
+    //Build file path
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+    // console.log(filePath);
+    // res.end();
+
+    //Extension of file
+    let extname = path.extname(filePath);
+
+    //Intial content type
+    let contentType = 'text/html';
+
+    // Check ext and set content type
+  switch (extname) {
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
+  }
+
+  //Read file
+  fs.readFile(filePath, (err, content) => {
+    if(err){
+        if(err.code == 'ENOENT'){
+            //Page not found
+            // console.log(filePath);
+            fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                  
+                res.end(content, 'utf8');
+            });
+        }else{
+            //Some other error
+            res.writeHead(500);  
+            res.end(`Server error ${err.code}`);
+        }
+    }else{
+        //Success
+        res.writeHead(200, {'Content-Type': contentType});  
+        res.end(content, 'utf8');
     }
+  });
 });
 
 const PORT = process.env.PORT || 5000;
